ada0a3cd8bb02197200ddc591715ef8724c324db
metacoding <getinthere@naver.com>
2023-03-19 03:18:48 +0900

    미완료

--- src/main/java/shop/mtcoding/hiberpc/controller/UserController.java
+++ src/main/java/shop/mtcoding/hiberpc/controller/UserController.java
@@ -8,6 +8,10 @@ import shop.mtcoding.hiberpc.model.UserRepository;
 @RestController
 public class UserController {
     private final UserRepository userRepository;
-
-
+    
+    public void join(){}
+    public void login(){}
+    public void userDetail(){}
+    public void userList(){}
+    public void like(){} // 좋아하는 숫자로 쿠키 학습
 }
